246233ed340a05a82f23d74d3f60847f68017194
TubbyStubby <tubbystubby2@gmail.com>
2022-03-13 20:09:22 +0530

    converted to messy ts project

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+import * as core from '@actions/core';
+import * as github from '@actions/github';
 
 try {
   // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
+  const nameToGreet: string = core.getInput('who-to-greet');
+  const time: string = (new Date()).toTimeString();
+
   console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
   core.setOutput("time", time);
+  
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  const payload: string = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
